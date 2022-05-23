2b0f76f1ff3d09adfe46babbe9dff3c6f5b26a7e
wly <1355498705@qq.com>
2022-05-23 19:56:24 +0800

    初始化

---
+++ app2.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+
+const app = express()
+
+app.get('/',(req,res) => {
+  res.send('hello word')
+})
+
+app.listen('3000',() => {
+  console.log('http://localhost:3000');
+})  
